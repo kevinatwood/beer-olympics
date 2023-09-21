f7f25f78213f055b2ef1817afe6817954a658ce2
lukehevey <lukehevey8@gmail.com>
2023-09-21 18:32:21 -0500

    Added a route for teams, edited signp screen

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const teamRoutes = require('./teamRoutes')
 
 router.use('/users', userRoutes);
+router.use('/teams', teamRoutes);
 // router.use('/projects', projectRoutes);
 
 module.exports = router;
