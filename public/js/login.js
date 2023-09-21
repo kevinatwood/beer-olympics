b8cf2f59a2bf671d6bf18512a48f4a99954fcf2b
kevinatwood <atwoodkevin22@gmail.com>
2023-09-20 21:25:56 -0500

    added psot route to create new user

--- public/js/login.js
+++ public/js/login.js
@@ -39,9 +39,10 @@ const signupFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert(response.statusText);
+      console.log(response);
+      alert("Enter a valid email and/or password");
     }
-  }
+  } 
 };
 
 document
