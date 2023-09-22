d3a203d413be7fe21ed054617ed5330694a4f6d7
kevinatwood <atwoodkevin22@gmail.com>
2023-09-21 19:06:16 -0500

    fixed teamPage routes

--- public/js/login.js
+++ public/js/login.js
@@ -37,7 +37,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/teamPage');
     } else {
       console.log(response);
       alert("Enter a valid email and/or password");
