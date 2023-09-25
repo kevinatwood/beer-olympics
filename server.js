4c8fe0103c1138ad5a3ae176187e871faf2f7473
Walter Moleterno <135560995+Boilermaker74@users.noreply.github.com>
2023-09-25 18:54:18 -0500

    Added Front end to backend

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const app = express();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+app.use(express.static(path.join(__dirname, 'public'))); //added middleware for static images 
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
