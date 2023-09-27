08d2c367be11a4b07a815e2253b958566512158c
lukehevey <lukehevey8@gmail.com>
2023-09-26 20:04:52 -0500

    Commented out possible code for standings.js

---
+++ public/js/standings.js
@@ -0,0 +1,35 @@
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
+
+// class Standings extends Model {}
+
+// Standings.init(
+//     {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   user_id: {
+//     type: DataTypes.INTEGER,
+//     references: {
+//       model: '',
+//       key: 'id',
+//     },
+//   },
+// },
+// {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'standings',
+//   }
+// );
+
+// module.exports = Standings;
