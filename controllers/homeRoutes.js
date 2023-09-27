91049c0a0906afddb3f7a470793b1cf871797dce
kevinatwood <atwoodkevin22@gmail.com>
2023-09-26 19:26:12 -0500

    added folder name to connect routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -107,6 +107,7 @@ router.get('/standings', async (req, res) => {
 
     // Serialize data so the template can read it
     const teams = teamData.map((team) => team.get({ plain: true }));
+    console.log(teams)
 
     // Pass serialized data and session flag into template
     res.render('standings', { 
